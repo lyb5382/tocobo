00e2ae80366ad9093c891c6d48c13f093c1c5123
lyb5382 <lyb538202@gmail.com>
2025-08-26 15:37:54 +0900

    j

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Hero from "./sections/Hero"
+import Brand from "./sections/Brand"
+import Cta from "./sections/Cta"
+import Reviews from "./sections/Reviews"
+import Products from "./sections/Products"
+import "./styles/main.scss"
+function App() {
+ 
+  return (
+    <>
+      <Header/>
+      <main>
+        <section id="Hero" className="Section">
+          <Hero/>
+        </section>
+        <section id="Brand" className="Section">
+          <Brand/>
+        </section>
+        <section id="Products" className="Section">
+          <Products/>
+        </section>
+        <section id="Reviews" className="Section">
+          <Reviews/>
+        </section>
+        <section id="Cta" className="Section">
+          <Cta/>
+        </section>
+      </main>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Nav from './Nav'
+import Util from './Util'
+import TopBanner from './TopBanner'
+import { headerData } from '../util/header'
+import "../styles/components/header.scss"
+const Header = () => {
+
+  const headerLogo = headerData.logo
+
+  return (
+    <div>
+      <TopBanner />
+
+      <header>
+        <div className="inner">
+          <Nav />
+          <h1 className="tit">
+            <a href={headerLogo.href}>
+              <img src={headerLogo.src} alt={headerLogo.alt} />
+            </a>
+          </h1>
+          <Util />
+        </div>
+        Header
+      </header>
+    </div>
+  )
+}
+
+export default Header
